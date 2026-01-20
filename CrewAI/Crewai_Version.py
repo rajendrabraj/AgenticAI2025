2d7f6bb775dbd69b459fa641e9b446d73420fd93
Raj_Agentic_AI <39874822+rajendrabraj@users.noreply.github.com>
2026-01-20 10:53:28 +0530

    Add files via upload

--- CrewAI/Crewai_Version.py
+++ CrewAI/Crewai_Version.py
@@ -10,8 +10,11 @@ def get_crew_ai_version():
 
 lc_version = get_crew_ai_version()
 print("Detected crewai version:", lc_version)
+print("-" * 90)
 # Helper to compare versions
 def version_tuple(v):
     return tuple(int(x) for x in v.split("."))
 
+
+print("-" * 90)
 
